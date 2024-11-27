58b4e1b1e40cca3c9fc67e1eded01934da618e5b
Janu <fefar@janu_patel.com>
2024-11-27 11:17:15 +0530

    change console

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import React, { Component } from 'react'
 import Navbar from './component/Navbar'
 import News from './component/News'
 
+comsole.log("hello world");
+
 export default class App extends Component {
+  
   render() {
     return (
       <div >
         <Navbar/>
         <News pageSize={5}/>
-        comsole.log("hello world");
+        
         
       </div>
     )
